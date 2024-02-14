6aafa19740be1560a861b47a87c494057563c4da
brayan martinez flores <brayan.mflores@hotmail.com>
2024-02-13 19:34:25 -0500

    [update] detalle sv v1

--- main.js
+++ main.js
@@ -4,3 +4,17 @@ onload = () => {
       clearTimeout(c);
     }, 1000);
   };
+  // determino el tamano de la pantalla	
+  var w = window.innerWidth;
+  var h = window.innerHeight;
+
+  // pinto 250 estrellas random
+  for (i = 0; i < 100; i++) {
+      wRan = Math.floor(Math.random() * w);
+      hRan = Math.floor(Math.random() * h);
+      var star = document.createElement("div");
+      star.setAttribute("class", "star");
+      star.style.bottom = hRan + "px";
+      star.style.right = wRan + "px";
+      document.body.appendChild(star);
+  } 
